497be176947a217faf174281fa20aeb3f936bf92
Harihsdiof <bookrock21@outlook.com>
2024-03-14 19:33:00 +0700

    Windelay

--- Assets/Script/WinScreen.cs
+++ Assets/Script/WinScreen.cs
@@ -1,11 +1,12 @@
 using System;
 using UnityEngine;
+using System.Collections;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class WinScreen : MonoBehaviour
 {
     public GameObject winScreenUI; // ระบุ UI Canvas ที่มีหน้าจอชนะ
-
+    public float delay;
     private void Start()
     {
         winScreenUI.SetActive(false);
@@ -15,8 +16,12 @@ public class WinScreen : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
             winScreenUI.SetActive(true);
-        {
-            
-        }
+        StartCoroutine(Countdown());
+    }
+
+    IEnumerator Countdown ()
+    {
+        yield return new WaitForSeconds (delay);
+        SceneManager.LoadScene(0);
     }
 }
