ddd4f55000d14ea9d3fb34690552aaa86bdc2d85
Harihsdiof <bookrock21@outlook.com>
2024-03-21 22:43:35 +0700

    Fixbugcredit

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -19,6 +19,10 @@ public class MainMenu : MonoBehaviour
    {
       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
    }
+   public void Credit()
+      {
+         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+      }
    public void Quit()
    {
       Application.Quit();
